ef87fc80856efb7f903797fe45d28e8129266c99
Yoshigamer360 <133695386+Yoshigamer360@users.noreply.github.com>
2024-06-20 19:12:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-# Chase the egg
-# Created by: Yoshi gamer 360
+# Created by: Yoshi
 
 import turtle, random, time
 
@@ -15,7 +14,6 @@ screen.title('Hungry Turtle Game by Yoshi Gamer 360')
 player = turtle.Turtle()
 player.shape('turtle')
 player.color('blue')
-colour = ['green, blue, purple']
 player.penup()
 
 player2 = turtle.Turtle()
@@ -23,17 +21,6 @@ player2.shape('turtle')
 player2.color('blue')
 player2.penup()
 player2.setheading(180)
-
-# Create the food turtle
-food = turtle.Turtle()
-food.shape('circle')
-food.color('yellow')
-food.penup()
-food.goto(random.randint((-width/2)+10, (width/2)-10),
-          random.randint((-height/2)+10, (height/2)-10))
-
-# Create the obstacles
-
 
 # Create obstacle turtle
 obstacle_1 = turtle.Turtle()
@@ -63,6 +50,16 @@ obstacle_4.color('white')
 obstacle_4.penup()
 obstacle_4.goto(random.randint((-width/2)+10, (width/2)-10),
                 random.randint(-240, -10))
+
+
+# Create the food turtle
+food = turtle.Turtle()
+food.shape('circle')
+food.color('yellow')
+food.penup()
+food.goto(random.randint((-width/2)+10, (width/2)-10),
+          random.randint((-height/2)+10, (height/2)-10))
+
 
 # Create the score turtle
 score = turtle.Turtle()
@@ -133,6 +130,38 @@ while True:
         score_value += 10
         score.clear()
         score.write(f'Score: {score_value}', align='left', font=('Courier', 18, 'normal'))
+        
+    elif player.distance(obstacle_1) < 20:
+        player.goto(0,0)
+        obstacle_1.goto(random.randint((-width/2)+10, (width/2)-10),
+                random.randint(-240, -10))
+        score_value -= 30
+        score.clear()
+        score.write(f'Score: {score_value}', align='left', font=('Courier', 18, 'normal'))
+    elif player.distance(obstacle_2) < 20:
+        player.goto(0,0)
+        obstacle_2.goto(random.randint((-width/2)+10, (width/2)-10),
+                random.randint(-240, -10))
+        score_value -= 30
+        score.clear()
+        score.write(f'Score: {score_value}', align='left', font=('Courier', 18, 'normal'))
+    elif player.distance(obstacle_3) < 20:
+        player.goto(0,0)
+        obstacle_3.goto(random.randint((-width/2)+10, (width/2)-10),
+                random.randint(-240, -10))
+        score_value -= 30
+        score.clear()
+        score.write(f'Score: {score_value}', align='left', font=('Courier', 18, 'normal'))
+    elif player.distance(obstacle_4) < 20:
+        player.goto(0,0)
+        obstacle_4.goto(random.randint((-width/2)+10, (width/2)-10),
+                random.randint(-240, -10))
+        score_value -= 30
+        score.clear()
+        score.write(f'Score: {score_value}', align='left', font=('Courier', 18, 'normal'))
+
+        
+
 
 
     if player2.distance(food) < 20:
@@ -141,6 +170,39 @@ while True:
         score2_value += 10
         score2.clear()
         score2.write(f'Score: {score2_value}', align='left', font=('Courier', 18, 'normal'))
+
+    elif player2.distance(obstacle_1) < 20:
+        player.goto(0,0)
+        score_value -= 30
+        obstacle_1.goto(random.randint((-width/2)+10, (width/2)-10),
+                random.randint(-240, -10))
+        score.clear()
+        score.write(f'Score: {score_value}', align='left', font=('Courier', 18, 'normal'))
+    elif player2.distance(obstacle_2) < 20:
+        player.goto(0,0)
+        score_value -= 30
+        obstacle_2.goto(random.randint((-width/2)+10, (width/2)-10),
+                random.randint(-240, -10))
+        score.clear()
+        score.write(f'Score: {score_value}', align='left', font=('Courier', 18, 'normal'))
+    elif player2.distance(obstacle_3) < 20 :
+        player.goto(0,0)
+        obstacle_3.goto(random.randint((-width/2)+10, (width/2)-10),
+                random.randint(-240, -10))
+        score_value -= 30
+        score.clear()
+        score.write(f'Score: {score_value}', align='left', font=('Courier', 18, 'normal'))
+    elif player2.distance(obstacle_4) < 20:
+        player.goto(0,0)
+        obstacle_4.goto(random.randint((-width/2)+10, (width/2)-10),
+                random.randint(-240, -10))
+        score_value -= 30
+        score.clear()
+        score.write(f'Score: {score_value}', align='left', font=('Courier', 18, 'normal'))
+
+
+
+        
     
     # Check for collisions with the walls
     x, y= player.position()
