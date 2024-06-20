c82a4cb3de5e82d22855eed9fc6b74fce663dbb1
Yoshigamer360 <133695386+Yoshigamer360@users.noreply.github.com>
2024-06-20 18:14:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -49,3 +49,43 @@ score2.hideturtle()
 score2.penup()
 score2.goto((-width/2)+10, (height/2)-495)
 score2.write('Score: 0', align='left', font=('Courier', 18, 'normal'))
+
+# Set up the key bindings
+def go_up():
+    player.setheading(90)
+
+def go_down():
+    player.setheading(270)
+
+def go_left():
+    player.setheading(180)
+
+def go_right():
+    player.setheading(0)
+
+def go_up2():
+    player2.setheading(90)
+
+def go_down2():
+    player2.setheading(270)
+
+def go_left2():
+    player2.setheading(180)
+
+def go_right2():
+    player2.setheading(0)
+
+screen.onkeypress(go_up, 'Up')
+screen.onkeypress(go_down, 'Down')
+screen.onkeypress(go_left, 'Left')
+screen.onkeypress(go_right, 'Right')
+screen.listen()
+
+screen.onkeypress(go_up2, 'w')
+screen.onkeypress(go_down2, 's')
+screen.onkeypress(go_left2, 'a')
+screen.onkeypress(go_right2, 'd')
+screen.listen()
+
+score_value = 0
+score2_value = 0
